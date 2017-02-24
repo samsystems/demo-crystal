6b2cb3075867bf77664a1f80cc8630818346f412
Yury <yurydgfuentes@gmail.com>
2017-02-24 10:11:13 -0500

    Add Common module and notification service to dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { DropdownModule } from 'ng2-bootstrap/dropdown';
 
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 
+import { NotificationService } from "../shared/services/notification.service";
+
+
 @NgModule({
   imports: [
+    CommonModule,
     DashboardRoutingModule,
-    DropdownModule
+    DropdownModule,
   ],
-  declarations: [ DashboardComponent ]
+  declarations: [ DashboardComponent ],
+  providers: [NotificationService]
 })
 export class DashboardModule { }
