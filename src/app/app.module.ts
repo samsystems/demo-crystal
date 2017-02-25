3185379ae92d1a12161bf5943ea7e73d596eb8b0
Carlos Osiel Rojas Velázquez <osiel8809@gmail.com>
2017-02-25 15:09:02 -0500

    Merge branch 'master' of https://github.com/samsystems/demo-crystal into feature/document-detail-flow
    Conflicts:
    	src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import {AppComponent} from './app.component';
 import {CoreModule} from './core/core.module';
 import {AppRoutingModule} from './app.routes';
 
-
 @NgModule({
   declarations: [
     AppComponent
