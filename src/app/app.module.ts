65aa609371d16beb3aadab50f122deb41da13d21
yoan89 <yoan.uria8909@gmail.com>
2017-02-25 10:09:00 -0500

    document detail added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {CoreModule} from './core/core.module';
 import {AppRoutingModule} from './app.routes';
+import { BriefcaseComponent } from './briefcase/briefcase.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BriefcaseComponent
   ],
   imports: [
     // Angular modules
