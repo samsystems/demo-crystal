5fc4051ff0af967c4ddf748fab6908808fb8e8c8
Yury <yurydgfuentes@gmail.com>
2017-03-04 17:52:34 -0500

    add notification components

---
+++ src/app/main/components/notification/notification-item/notification-item.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {Document} from '../../../../models/document';
+
+@Component({
+  selector: 'app-notification-item',
+  templateUrl: 'notification-item.component.html',
+  styleUrls: ['notification-item.component.css']
+})
+export class NotificationItemComponent implements OnInit {
+  @Input('item') document: Document;
+  @Output() selectItem = new EventEmitter<Document>();
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/main/components/notification/notification-list/notification-list.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit, Input} from '@angular/core';
+import {Document} from '../../../../models/document';
+
+@Component({
+  selector: 'app-notification-list',
+  templateUrl: 'notification-list.component.html',
+  styleUrls: ['notification-list.component.css']
+})
+export class NotificationListComponent implements OnInit {
+
+  @Input('items') documents: Document[];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
