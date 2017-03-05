2d397c7e961b57b25b98c38a766b8b3b3da40b46
Yury <yurydgfuentes@gmail.com>
2017-03-05 10:03:44 -0500

    ad mechanism to update title

--- src/app/main/components/inbox/inbox.component.ts
+++ src/app/main/components/inbox/inbox.component.ts
@@ -8,29 +8,60 @@ import {DocumentService} from "../../../services/document.service";
   styleUrls: ['./inbox.component.css']
 })
 export class InboxComponent implements OnInit , OnDestroy{
-
+  title: string;
   documents: any;
-  sub:any;
+  subQueryParams:any;
+  tag: string;
 
   constructor(private documentService: DocumentService, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
 
-    this.sub = this.route.queryParams.subscribe(params => {
-      // Defaults to -1 if no query param provided.
-      let status = +params['status'] || -1;
+    this.title = 'Inbox';
 
-      this.documents = this.documentService.getDocumentsByStatus(status);
+    this.subQueryParams = this.route.queryParams.subscribe(params => {
+      this.tag = null;
 
+      if (params['tag']) {
+        this.getDocumentsByTag(params['tag']);
+      } else {
+        // Defaults to -1 if no query param provided.
+        let status = params['status'] || -1;
+        this.getDocumentByStatus(+status);
+      }
     });
+  }
 
+  getDocumentByStatus(status: number):void{
 
+    //Setting title
+    switch(status){
+      case -1:
+        this.title = 'Inbox';
+        break;
+      case 0:
+        this.title = 'Draft Documents';
+        break;
+      case 1:
+        this.title = 'Pending Approval Documents';
+        break;
+      case 2:
+        this.title = 'Approved Documents';
+        break
+    }
 
+    this.documents = this.documentService.getDocumentsByStatus(status);
+  }
+
+  getDocumentsByTag(tag: string): void {
+    this.tag = tag;
+    this.title = 'Documents with tag';
+    this.documents = this.documentService.getDocumentsByTag(tag)
   }
 
   ngOnDestroy() {
-    this.sub.unsubscribe();
+    this.subQueryParams.unsubscribe();
   }
 
 }
