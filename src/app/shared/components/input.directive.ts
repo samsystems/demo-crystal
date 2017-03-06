06bdb21d36147936ff7ffed2aa93ef756c986752
yoan89 <yoan.uria8909@gmail.com>
2017-03-06 17:43:16 -0500

    input container

---
+++ src/app/shared/components/input.directive.ts
@@ -0,0 +1,17 @@
+import {Directive, Renderer, ElementRef, Optional} from '@angular/core';
+import {NgControl} from '@angular/forms';
+
+@Directive({
+  selector: `input[bsInput], textarea[bsInput], input[matInput], textarea[bsInput]`,
+  host: {
+    '[class.form-control]': 'true'
+  }
+})
+export class InputDirective {
+
+  constructor(private _elementRef: ElementRef,
+              private _renderer: Renderer,
+              @Optional() public _ngControl: NgControl) {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {DocEditorComponent} from './components/doc-editor/doc-editor.component';
 import {InputTagComponent} from './components/input-tag/input-tag.component';
+import {InputContainerComponent} from './components/input-container/input-container.component';
+import {InputDirective} from './components/input.directive';
 
 @NgModule({
   imports: [CommonModule],
-  exports: [DocEditorComponent, InputTagComponent],
-  declarations: [DocEditorComponent, InputTagComponent],
+  exports: [InputTagComponent, InputContainerComponent, InputDirective],
+  declarations: [InputTagComponent, InputContainerComponent, InputDirective],
   providers: [],
 })
 export class SharedModule {
