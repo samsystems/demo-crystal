2cd11c5bbf061a48c9689a75507b0b695dab361b
Yury <yurydgfuentes@gmail.com>
2017-03-06 10:39:17 -0500

    add subscription to document observer

--- src/app/main/components/main/main.component.ts
+++ src/app/main/components/main/main.component.ts
@@ -2,14 +2,13 @@ import {Component, OnInit, OnDestroy} from '@angular/core';
 
 import {AuthService} from '../../../core/services/auth.service';
 import {DocumentService} from "../../../services/document.service";
-import {Status} from "../../../models/document";
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, OnDestroy {
   tags: any;
   tagsKeys: any;
 
@@ -23,6 +22,9 @@ export class MainComponent implements OnInit {
   draft: number;
   pendingApproval: number;
   approved: number;
+
+  //Subscriptions
+  documentSubscription: any;
 
   constructor(private auth: AuthService, private documentService: DocumentService) {
     this.primaryResponsibilities = 0;
@@ -38,7 +40,7 @@ export class MainComponent implements OnInit {
 
   ngOnInit() {
     this.syncData(this.documentService.findAll());
-    this.documentService.getDocuments().subscribe((documents) => {
+    this.documentSubscription = this.documentService.getDocuments().subscribe((documents) => {
       this.syncData(documents);
     });
   }
@@ -53,5 +55,9 @@ export class MainComponent implements OnInit {
     this.draft = this.documentService.getDraftDocuments().length;
     this.pendingApproval = this.documentService.getPending_ApprovalDocuments().length;
     this.approved = this.documentService.getApprovedDocuments().length;
+  }
+
+  ngOnDestroy() {
+    this.documentSubscription.unsubscribe();
   }
 }
